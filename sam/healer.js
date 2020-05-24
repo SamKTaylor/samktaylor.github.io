4400aac5bfd7c9cb76cb1c2fb0bca0718d64fd0d
Sam Taylor <sam@spapartsvortex.eu>
2020-05-24 15:11:35 +0100

    Updates, smart move, message commands

---
+++ sam/healer.js
@@ -0,0 +1,125 @@
+var CharState = "Combat"
+var TargetCaller = "McGreebTanks"
+
+var HealAmount = 160;
+var PartyHealAmount = 500;
+
+function on_cm(from, message){
+
+    if(from == TargetCaller){
+        TargetCallerCommand(message);
+    }
+    
+}
+
+const TargetCallerCommand = (message) => {
+
+    switch (message.action) {
+        case "smart_move":
+            smart_move({x: message.x, y: message.y}, Set_Previous_CharState);
+            break;
+ 
+        default:
+            //unknown message
+    }
+
+}
+
+function Set_Previous_CharState(){ CharState = PreviousCharState; }
+
+
+function Get_Party_Target() {
+    Tank = get_player(TargetCaller);
+    var target = get_monster(Tank.target);
+    return target;
+}
+
+function Stay_Near_Tank(target) {
+    if (!is_in_range(target)) {
+        move(target.real_x - 30, target.real_y);
+    }
+}
+
+function Heal_Tank(Tank) {
+    if ((Tank.max_hp - Tank.hp) > HealAmount) {
+        if ((Tank.max_hp - Tank.hp) > HealAmount + PartyHealAmount) {
+            use_skill('partyheal');
+            heal(Tank);
+        } else {
+            heal(Tank);
+        }
+        heal(Tank);
+        //if(character.mp > 400)
+        //{
+        //use_skill("partyheal");
+        //}
+    }
+}
+
+function CheckHpMP() {
+    if (!character.rip) {
+        if (character.max_hp - character.hp > 150 || character.max_mp - character.mp > 150) {
+            use_hp_or_mp();
+        }
+    }
+}
+
+function DoCombat() {
+    var Tank = get_player(TargetCaller);
+
+    CheckHpMP();
+    if (Tank) {
+        Stay_Near_Tank(Tank);
+        Heal_Tank(Tank);
+    }
+
+    var target = get_targeted_monster();
+
+    if (!target) {
+        target = Get_Party_Target();
+        if (character.mp > 400) {
+            use_skill('curse', target);
+        }
+    }
+
+}
+
+setInterval(function () {
+
+
+    if (!character.rip) {
+        if (!is_on_cooldown("regen_mp") && character.mp < character.max_mp - 100) {
+            use_skill("regen_mp");
+        } else {
+            if (!is_on_cooldown("regen_hp") && character.hp < character.max_hp - 100) {
+                use_skill("regen_hp");
+            }
+        }
+    }
+
+    Tank = get_player(TargetCaller);
+    Stay_Near_Tank(Tank)
+
+    for (id in parent.party) {
+        var current = get_entity(id);
+        if (typeof current != "undefined") {
+            Heal_Tank(current);
+        }
+
+    }
+
+
+    loot();
+    set_message(CharState);
+    switch (CharState) {
+        case "Combat":
+            //DoCombat();
+            break;
+        case "HealthRegen":
+            //DoHeal();
+            break;
+        default:
+            CharState = "Combat";
+    }
+
+}, 1000 / 4); // Loops every 1/4 seconds.
--- sam/warrior.js
+++ sam/warrior.js
@@ -1,165 +1,214 @@
-var attack_mode=true
+var attack_mode = true
 
-var TargetLastX=0
-var TargetLastY=0
+var TargetLastX = 0
+var TargetLastY = 0
 
-var CharState="Combat"
-var PreviousCharState="Idle"
+var CharState = "Combat"
+var PreviousCharState = "Idle"
 
 var AssistTarget = null
 
-var BattleX=643;
-var BattleY=1819;
+var BattleX = 643;
+var BattleY = 1819;
 
-var HealthSafetyPercentage=60;
+var HealthSafetyPercentage = 60;
 
-var DepositLimit=200000;
+var DepositLimit = 200000;
 
 var log = true;
 
-var max_attack = 100;
+var max_attack = 400;
 
-function RegenSkills(){
-	if(!character.rip)	
-	{
-		if(!is_on_cooldown("regen_mp") && character.mp < character.max_mp - 100){
-			use_skill("regen_mp");
-		}else{
-			if(!is_on_cooldown("regen_hp") && character.hp < character.max_hp - 100){
-				use_skill("regen_hp");
-			}
-		}
-	}
+const SMART_MOVE_DISTANCE = 200;
+
+const CRABS = {
+    "x": -1195.2656321693682,
+    "y": -156.6394604905829,
+    "map": "main"
+};
+
+const BANK = {
+    "x": 164.42326127218746,
+    "y": -137.7273980939667,
+    "map": "main"
+};
+
+const BANK_INTERNAL = {
+    "x": 1.026189346389918,
+    "y": -72.89611622120091,
+    "map": "bank"
+};
+
+const distanceFrom = (x, y) => {
+    return Math.abs(Math.sqrt(
+        Math.pow(x - character.real_x, 2) + Math.pow(y - character.real_y, 2)
+    ));
+};
+
+const moveTo = (x, y) => {
+
+    draw_line(x,y,character.x,character.y);
+
+    if(distanceFrom(x,y) > SMART_MOVE_DISTANCE){
+
+        cm_party({action:"smart_move", x: x, y: y});
+
+        PreviousCharState = CharState;
+        CharState = "Calculating";
+        smart_move({x: x, y: y}, Set_Previous_CharState);
+    }else{
+        move(x,y);
+    }
+}
+
+function Set_Previous_CharState(){ CharState = PreviousCharState; }
+
+const cm_party = (message) => {
+
+    for (id in parent.party) {
+        var current = get_entity(id);
+        if (typeof current != "undefined") {
+            send_cm(McGreebHeals, message);
+        }
+    }
+    
+}
+
+function RegenSkills() {
+    if (!character.rip) {
+        if (!is_on_cooldown("regen_mp") && character.mp < character.max_mp - 100) {
+            use_skill("regen_mp");
+        } else {
+            if (!is_on_cooldown("regen_hp") && character.hp < character.max_hp - 100) {
+                use_skill("regen_hp");
+            }
+        }
+    }
 }
 
 
-function Get_New_Target()
-{
+function Get_New_Target() {
     //Check for enemys attacking the group allready
 
     var highest_xp = null;
 
-	for(id in parent.entities)
-    {
-        var current=parent.entities[id];
-        if(current.type!="monster" || !current.visible || current.dead) continue;
+    for (id in parent.entities) {
+        var current = parent.entities[id];
+        if (current.type != "monster" || !current.visible || current.dead) continue;
 
-        if(highest_xp != null){
-            if(current.xp > highest_xp.xp){
-                if(current.attack < max_attack){
+        if (highest_xp != null) {
+            if (current.xp > highest_xp.xp) {
+                if (current.attack < max_attack) {
                     highest_xp = current;
                 }
             }
-        }else{
+        } else {
             highest_xp = current;
         }
 
-        if('target' in current){
+        if ('target' in current) {
             var us = AllPartyNameArray();
-            if(us.includes(current.target)){
+            if (us.includes(current.target)) {
                 return current;
             }
         }
     }
 
     //non found get_nearest_monster
-    if(highest_xp != null){
+    if (highest_xp != null) {
         return highest_xp;
-    }else{
+    } else {
         return get_nearest_monster();
     }
-    
+
 }
 
-function DoCombat()
-{	
-	
-	if(!attack_mode || character.rip || is_moving(character)) return;
-	
-	var target=get_targeted_monster();
-	
-	if(!target)
-	{
-		target = Get_New_Target();
-		if (target)
-		{
-			TargetLastX = target.x;
-			TargetLastY = target.y;
-		}
-	}
-	
-	if(!target)
-	{
-		CharState="ReturnToBattle";
-		return;
-	}
-	
-	if(!is_in_range(target))
-	{
-        if(!is_on_cooldown("charge")){
+function DoCombat() {
+
+    if (!attack_mode || character.rip || is_moving(character)) return;
+
+    var target = get_targeted_monster();
+
+    change_target(target);
+
+    if (!target) {
+        target = Get_New_Target();
+        if (target) {
+            TargetLastX = target.x;
+            TargetLastY = target.y;
+        }
+    }
+
+    if (!target) {
+        CharState = "ReturnToBattle";
+        return;
+    }
+
+    if (!is_in_range(target)) {
+        if (!is_on_cooldown("charge")) {
             use_skill("charge");
         }
-		move(
-			target.x,
-			target.y
-			);
-	} else if(can_attack(target)) {
-        if(!is_on_cooldown("taunt")){
+        moveTo(
+            target.x,
+            target.y
+        );
+    } else if (can_attack(target)) {
+        if (!is_on_cooldown("taunt")) {
             use_skill("taunt", target);
         }
         attack(target);
-	}
+    }
 }
 
 //Return array containing names of all party members
-function AllPartyNameArray(){
+function AllPartyNameArray() {
 
     var array = [];
 
-    for(id in parent.party)
-    {
+    for (id in parent.party) {
         var current = get_entity(id);
-		if(typeof current != "undefined"){
+        if (typeof current != "undefined") {
             array.push(current.name);
-		}
+        }
 
     }
-	
+
     return array;
 }
 
 //Return array containing names of all other party members
-function OtherPartyNameArray(){
+function OtherPartyNameArray() {
 
     var array = [];
 
-    for(id in parent.party)
-    {
+    for (id in parent.party) {
         var current = get_entity(id);
-		if(typeof current != "undefined"){
-			if(!current.me){
-            	array.push(current.name);
-        	}
-		}
+        if (typeof current != "undefined") {
+            if (!current.me) {
+                array.push(current.name);
+            }
+        }
     }
-	
+
     return array;
 }
 
 //check the target of all nearby monsters, if they are targeting a party memeber set state to assist and target to that monster
-function CheckAgro(){
+function CheckAgro() {
 
-	var us = OtherPartyNameArray();
+    var us = OtherPartyNameArray();
     target = null;
-	for(id in parent.entities)
-    {
-        var current=parent.entities[id];
-        if(current.type!="monster" || !current.visible || current.dead) continue;
+    for (id in parent.entities) {
+        var current = parent.entities[id];
+        if (current.type != "monster" || !current.visible || current.dead) continue;
 
-        if('target' in current){
-            if(us.includes(current.target)){
+        if ('target' in current) {
+            if (us.includes(current.target)) {
                 console.log(current.target + " Needs Help!");
                 target = current;
+
+                change_target(target);
+
                 setCharState("Assist");
             }
 
@@ -168,54 +217,60 @@ function CheckAgro(){
 }
 
 //Charge down the monster and taunt it of you can, the go back to combat mode.
-function DoAssist(){
-	
-    if(!is_on_cooldown("charge")){
+function DoAssist() {
+
+    if(target == null){
+        setCharState("Combat");
+        exit;
+    }
+
+    if (!is_on_cooldown("charge")) {
         use_skill("charge");
     }
-    
-    move(
+
+    moveTo(
         target.x,
         target.y
     );
 
-    if(can_attack(target)) {
-        if(!is_on_cooldown("taunt")){
+    if (can_attack(target)) {
+        if (!is_on_cooldown("taunt")) {
             use_skill("taunt", target);
         }
         attack(target);
-        
+
         setCharState("Combat");
     }
-        	
+
 }
 
-function setCharState(state){
+function setCharState(state) {
     CharState = state;
     set_message(CharState);
 }
 
-setInterval(function(){
-	
-	CheckAgro();
-		
+setInterval(function () {
+
+    CheckAgro();
+
+    send_cm("McGreebHeals", ["arse"]);
+
     loot();
-   
-	switch(CharState){
 
-		case "Assist":
-			DoAssist();
+    switch (CharState) {
+
+        case "Assist":
+            DoAssist();
             break;
-        
-		case "Combat":
-			DoCombat();
+
+        case "Combat":
+            DoCombat();
             break;
-                        
-		default:
-		      setCharState("Combat");
-	}
-	
-	RegenSkills();
 
-},1000/4); // Loops every 1/4 seconds.
+        default:
+            setCharState("Combat");
+    }
 
+    RegenSkills();
+
+}, 1000 / 4); // Loops every 1/4 seconds.
