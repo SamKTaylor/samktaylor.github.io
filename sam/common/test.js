915b3f4eb01834402d21102f02277b7843bbdf59
Sam Taylor <sam@spapartsvortex.eu>
2020-05-24 19:52:05 +0100

    Another Test function

--- sam/common/test.js
+++ sam/common/test.js
@@ -1,1 +1,10 @@
-alert("hello");
+window.send_cm_party = (message) => {
+
+    for (id in parent.party) {
+        var current = get_entity(id);
+        if (typeof current != "undefined") {
+            send_cm(current.name, message);
+        }
+    }
+    
+}
