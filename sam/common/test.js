bdb463fafb7e0108884be62e11c426a1c252f0cf
Sam Taylor <sam@spapartsvortex.eu>
2020-05-24 19:15:46 +0100

    Added Test Library

---
+++ sam/common/test.js
@@ -0,0 +1,10 @@
+const cm_party = (message) => {
+
+    for (id in parent.party) {
+        var current = get_entity(id);
+        if (typeof current != "undefined") {
+            send_cm(current.name, message);
+        }
+    }
+    
+}
