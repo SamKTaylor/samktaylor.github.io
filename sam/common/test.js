32fd86b5db6d68c92e5fd8d2274432898c528930
Sam Taylor <sam@spapartsvortex.eu>
2020-05-24 19:45:20 +0100

    Testing Alert Hello

--- sam/common/test.js
+++ sam/common/test.js
@@ -1,10 +1,1 @@
-window.bots.cm_party = (message) => {
-
-    for (id in parent.party) {
-        var current = get_entity(id);
-        if (typeof current != "undefined") {
-            send_cm(current.name, message);
-        }
-    }
-    
-}
+alert("hello");
