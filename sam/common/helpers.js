f5ae70a32d1b6d754f50a345c796fa0aae02c241
Sam Taylor <sam@spapartsvortex.eu>
2020-05-25 12:22:32 +0100

    testing helpers

--- sam/common/helpers.js
+++ sam/common/helpers.js
@@ -6,67 +6,64 @@ const SMART_MOVE_DISTANCE = 200;
 
 log('HELPERS LOADED');
 
-window.helpers = function (t) {
+window.helpers = function () {
 
-window.helpers.distanceFrom = (x, y) => {
-    return Math.abs(Math.sqrt(
-        Math.pow(x - character.real_x, 2) + Math.pow(y - character.real_y, 2)
-    ));
-};
+    this.distanceFrom = (x, y) => {
+        return Math.abs(Math.sqrt(
+            Math.pow(x - character.real_x, 2) + Math.pow(y - character.real_y, 2)
+        ));
+    };
 
-window.helpers.moveTo = (x, y) => {
+    this.moveTo = (x, y) => {
 
-    draw_line(x,y,character.x,character.y);
+        draw_line(x,y,character.x,character.y);
 
-    if(window.helpers.distanceFrom(x,y) > SMART_MOVE_DISTANCE){
+        if(this.distanceFrom(x,y) > SMART_MOVE_DISTANCE){
 
-        window.helpers.send_cm_party({action:"smart_move", x: x, y: y});
+            this.send_cm_party({action:"smart_move", x: x, y: y});
 
-        PreviousCharState = CharState;
-        CharState = "Calculating";
-        smart_move({x: x, y: y});
-    }else{
-        move(x,y);
-    }
-}
-
-window.test = () => {
-    alert('test');
-}
-
-//Return array containing names of all party members
-window.helpers.getPartyNameArray = () => {
-
-    var array = [];
-
-    for (name in parent.party) {
-        array.push(name);
+            PreviousCharState = CharState;
+            CharState = "Calculating";
+            smart_move({x: x, y: y});
+        }else{
+            move(x,y);
+        }
     }
 
-    return array;
-}
+    //Return array containing names of all party members
+   this.getPartyNameArray = () => {
 
-//Return array containing names of all other party members
-window.helpers.getOtherPartyNameArray = () => {
+        var array = [];
 
-    var array = [];
-
-    for (name in parent.party) {
-        if (name != character.name) {
+        for (name in parent.party) {
             array.push(name);
         }
+
+        return array;
     }
 
-    return array;
-}
+    //Return array containing names of all other party members
+    this.getOtherPartyNameArray = () => {
 
-window.helpers.send_cm_party = (message) => {
+        var array = [];
 
-    for (id in parent.party) {
-        var current = get_entity(id);
-        if (typeof current != "undefined") {
-            send_cm(current.name, message);
+        for (name in parent.party) {
+            if (name != character.name) {
+                array.push(name);
+            }
         }
+
+        return array;
     }
-    
+
+    this.send_cm_party = (message) => {
+
+        for (id in parent.party) {
+            var current = get_entity(id);
+            if (typeof current != "undefined") {
+                send_cm(current.name, message);
+            }
+        }
+        
+    }
 }
