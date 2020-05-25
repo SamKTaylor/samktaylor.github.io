b171114c6585fdbfcea4c289819b2b03705ba142
Sam Taylor <sam@spapartsvortex.eu>
2020-05-25 12:17:57 +0100

    testing helpers

--- sam/common/helpers.js
+++ sam/common/helpers.js
@@ -2,29 +2,25 @@
 
 const SMART_MOVE_DISTANCE = 200;
 
-
-
-
-
 */
 
 log('HELPERS LOADED');
 
-window.helpers = {};
+window.helpers = function (t) {
 
-const distanceFrom = (x, y) => {
+window.helpers.distanceFrom = (x, y) => {
     return Math.abs(Math.sqrt(
         Math.pow(x - character.real_x, 2) + Math.pow(y - character.real_y, 2)
     ));
 };
 
-const moveTo = (x, y) => {
+window.helpers.moveTo = (x, y) => {
 
     draw_line(x,y,character.x,character.y);
 
-    if(distanceFrom(x,y) > SMART_MOVE_DISTANCE){
+    if(window.helpers.distanceFrom(x,y) > SMART_MOVE_DISTANCE){
 
-        cm_party({action:"smart_move", x: x, y: y});
+        window.helpers.send_cm_party({action:"smart_move", x: x, y: y});
 
         PreviousCharState = CharState;
         CharState = "Calculating";
@@ -53,7 +49,7 @@ window.helpers.getPartyNameArray = () => {
 }
 
 //Return array containing names of all other party members
-export function getOtherPartyNameArray() {
+window.helpers.getOtherPartyNameArray() {
 
     var array = [];
 
@@ -66,7 +62,7 @@ export function getOtherPartyNameArray() {
     return array;
 }
 
-window.send_cm_party = (message) => {
+window.helpers.send_cm_party = (message) => {
 
     for (id in parent.party) {
         var current = get_entity(id);
