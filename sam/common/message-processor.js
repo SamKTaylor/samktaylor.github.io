b7efbdd0667e7bbda5aa79e28bed3f07a24e8146
Sam Taylor <sam@spapartsvortex.eu>
2020-05-25 18:01:38 +0100

    make window function

--- sam/common/message-processor.js
+++ sam/common/message-processor.js
@@ -4,28 +4,24 @@ const TARGET_CALLER = "McGreebTanks";
 
 */
 
+window.cm_processor = {}
 
-function on_cm(from, message){
+window.cm_processor.processMessage = (from, message) => {
 
-    log("Message Recived.");
+    log("Processing Message.")
+
     if(from == TARGET_CALLER){
-		processMessage(message);
-    }
-    
-}
+        switch (message.action) {
+            case "smart_move":
+                smart_move({x: message.x, y: message.y});
+                break;
+            case "attack_this":
+                //TODO
 
-const processMessage = (message) => {
-
-    switch (message.action) {
-        case "smart_move":
-            smart_move({x: message.x, y: message.y});
-            break;
-        case "attack_this":
-            //TODO
-
-            break;
-        default:
-            //unknown message
+                break;
+            default:
+                //unknown message
+        }
     }
 
 }
